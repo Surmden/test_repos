70107947bebcacba0053ab6f5c5d1dd9f6f589cb
Surmden <surmanidzedenis609@gmail.com>
2021-10-07 20:55:05 +0300

    Chlen

---
+++ arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace массивы_в_циклах
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 1, 3, 7, 12 };
+            int b = a.Length;
+            for (int i = 0; i < b; i++)
+            {
+                Console.Write(a[i]);
+                
+                Console.Write("  ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
